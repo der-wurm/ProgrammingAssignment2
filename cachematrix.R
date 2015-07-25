66128f37e9f7f941af37c1197cde41807e50e822
der-wurm <andrea.h.o.l.z@arcor.de>
2015-07-25 15:27:33 +0200

    final version for submission ... i hope ...

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,39 @@
 
-## caching the inverse of a matrix so that it doesn't have to be calculated again if it is known already
+## caching the inverse of a matrix
+## so that it doesn't have to be calculated again if it is known/calculated already
 
-
-## create a matrix with its getter and setter functions, return a list of them
+## create the getter and setter functions for the given matrix, return a list of the functions
+## fetches a matrix as argument (here: square and invertible - otherwise "solve" doesn't work in cacheSolve)
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL                                # initialize the inverse
-  set <- function(y){
-    x <<- y       # copy matrix
-    inv <<- NULL
-  }
-  get <- function() x                        # return matrix
-  setInv <- function(invX) inv <<- invX      # set inverse to given argument
-  getInv <- function() inv                   # return inverse
+  inv <- NULL                                           # initialize the inverse
   
-  list(set=set, get=get, setInv=setInv, getInv=getInv)
+  # create getters and setters
+  set <- function(y){                                   # set matrix to given argument y (not needed here)
+    x <<- y                                             ## copy matrix
+    inv <<- NULL                                        ## reset the inverse matrix
+  }
+  get <- function() x                                   # return matrix
+  setInv <- function(invX) inv <<- invX                 # set inverse matrix to given argument invX
+  getInv <- function() inv                              # return inverse
+  
+  list(set=set, get=get, setInv=setInv, getInv=getInv)  # return value (list of 4 functions)
 }
 
 
-## get the invertible of the existing matrix, get it from "cache" or calculate it this (first) time
+## get the inverse of the existing matrix - from "cache" or calculate it this (first) time
+## fetches a list of functions as argument (result from makeCacheMatrix)
 cacheSolve <- function(x, ...) {
-  inv <- x$getInv()  ## look for inverse, fetch value, returns NULL if not calculated yet
-  if(!is.null(inv)){
+  inv <- x$getInv()   ## fetch inverse matrix, returns NULL if not calculated yet
+  
+  if(!is.null(inv)){  ## already calculated
     message("already calculated, fetched from cache: ")
-    return(inv)
+    return(inv)       ## return existing inverse matrix
   }
-  data <- x$get()     ## fetch matrix for calculation
+  
+  # if not yet calculated:
+  data <- x$get()     ## fetch (original) matrix for calculation
   message("I need to calculate, wait a minute ... ;)")
-  inv <- solve(data)  ## calculate inverse (b can be left out then)
+  inv <- solve(data)  ## calculate inverse (2nd argument b can be left out)
   x$setInv(inv)       ## save / put matrix in "cache"
   inv                 ## Return a matrix that is the inverse of 'x'
 }
